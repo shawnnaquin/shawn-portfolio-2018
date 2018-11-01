7ff5536132aeea0aabb575448601a32989f6c537
shawnnaquin <shawn.naquin@gmail.com>
2018-11-01 17:58:54 -0500

    moved chunk, created alias for project page, added catch-all to router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+const Portfolio = () => import(/* webpackChunkName: "portfolio" */ "./views/Portfolio.vue");
+const Project = () => import(/* webpackChunkName: "project" */ "./views/Project.vue");
 
 Vue.use(Router);
 
@@ -8,11 +10,13 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
+
     {
       path: "/",
       name: "home",
       component: Home
     },
+
     {
       path: "/website",
       name: "website",
@@ -20,20 +24,32 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       // man I love this feature of vue!
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/Portfolio.vue")
+      component: Portfolio
     },
+
     {
       path: "/marketing",
       name: "marketing",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/Portfolio.vue")
+      component: Portfolio
     },
+
     {
       path: "/interactive",
       name: "interactive",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/Portfolio.vue")
+      component: Portfolio
     },
+
+    {
+      path: '/projects/:project',
+      component: Project,
+      alias: ['/marketing/:project', '/interactive/:project', '/website/:project' ]
+    },
+
+    {
+      path: "/*",
+      name: "home",
+      component: Home
+    }
+
   ]
 });
