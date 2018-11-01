d10e02f5a6a09651d63b6d0d0a9a95d848d465ed
shawnnaquin <shawn.naquin@gmail.com>
2018-11-01 18:00:06 -0500

    added project vue

---
+++ src/views/Project.vue
@@ -0,0 +1,60 @@
+<template>
+
+  <div class="project">
+
+  	<h1>Some Name of Project</h1>
+  	<h3>some tag line</h3>
+
+  	<div :class=" 'images' " >
+  		<template v-for="image in cats">
+  			<img :src="'//www.lorempixel.com/' + image[0] + '/' + image[1] + '/cats' " alt="cats" />
+  		</template>
+  	</div>
+
+  	<p>
+  		description of the project!
+  	</p>
+
+  </div>
+
+</template>
+
+<script>
+
+	export default {
+		data() {
+			return {
+				cats: [],
+			}
+		},
+		mounted() {
+			this.setCats();
+		},
+		methods: {
+			getRandomSize(min, max) {
+				return Math.round( Math.random() * (max - min) + min );
+			},
+			setCats() {
+				for (var i = 0; i < 25; i++) {
+				  var width = this.getRandomSize(200, 400);
+				  var height =  this.getRandomSize(200, 400);
+				  this.cats.push( [width, height] )
+				}
+			}
+		}
+	};
+
+</script>
+
+<style lang="scss" scoped >
+	.images {
+		line-height: 0;
+		column-count: 5;
+		column-gap: 0;
+		column-count: 3;
+	}
+	img {
+		width: 100% !important;
+		height: auto !important;
+	}
+</style>
