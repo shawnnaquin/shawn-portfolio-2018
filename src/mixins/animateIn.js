93df05da1eba89d8ebc757e74b31a57c11efadec
shawnnaquin <shawn.naquin@gmail.com>
2018-11-06 21:47:47 -0600

    - animateIn mixin from after move;

---
+++ src/mixins/animateIn.js
@@ -0,0 +1,46 @@
+export default {
+
+	data() {
+		return {
+			frame: false
+		}
+	},
+
+	beforeDestroy() {
+		cancelAnimationFrame( this.frame );
+	},
+
+	methods: {
+
+		beforeEnter: function (el) {
+		  el.style.opacity = 0;
+		},
+		enter: function (el, done) {
+
+		  let delay = el.dataset.index * 200;
+		  // console.log(delay);
+		  setTimeout( () => {
+
+		  	let f = 0;
+		  	let fr = this.frame = requestAnimationFrame( t );
+
+		  	function t() {
+		  		if ( el.style.opacity < 1 )  {
+		  			requestAnimationFrame( t );
+		  			el.style.opacity = f += 0.08;
+		  		} else {
+		  			cancelAnimationFrame( fr );
+		  			el.style.opacity = 1;
+		  			done;
+		  			return;
+		  		}
+		  	}
+
+		  }, delay );
+
+		},
+		leave: function (el, done) {
+			done;
+		}
+	}
+};
