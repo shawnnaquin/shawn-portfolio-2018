36f491b5c7a6a463412069dad13857d9643b90d2
Shawn Naquin <shawn.naquin@gmail.com>
2018-11-07 18:00:58 -0600

    - system font family;

--- src/App.vue
+++ src/App.vue
@@ -82,8 +82,9 @@ export default {
   }
 
   #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
     -moz-osx-font-smoothing: grayscale;
     text-align:left;
     color: #2c3e50;
