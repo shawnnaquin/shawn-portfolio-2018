08c5e360a21ca3f0b3cfe1974708056dbb50fe99
Shawn Naquin <shawn.naquin@gmail.com>
2018-11-13 17:06:52 -0600

    - play icon that's unused so far;

---
+++ src/components/icons/play.vue
@@ -0,0 +1,11 @@
+<template>
+	<svg :class="['white']" xmlns="http://www.w3.org/2000/svg" version="1" viewBox="0 0 1000 1000">
+	  <path d="M500 990a490 490 0 1 1 0-980 490 490 0 0 1 0 980zm0-891A385 385 0 0 0 99 500c0 235 166 401 401 401s401-167 401-401c0-235-166-401-401-401zM366 277l357 223-357 223V277z"/>
+	</svg>
+</template>
+
+<style lang="scss" scoped >
+	.white {
+		fill: white;
+	}
+</style>
