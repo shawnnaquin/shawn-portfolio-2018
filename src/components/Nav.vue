4da3dc189f6349ca5c38371d0e4494f5ca54f88b
Shawn Naquin <shawn.naquin@gmail.com>
2018-11-14 13:03:20 -0600

    - refactored and made mobile nav, plus hover;

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,111 +1,277 @@
 <template>
 
-  <nav id="nav" class="">
+	<div>
 
-    <!-- <small>Project Type:</small> -->
+	<button :class="[ 'external', { ['menu-open']: menuOpen } ]" @click="button" >Menu</button>
 
-    <ul>
-      <transition name="fade">
-        <li v-if="$route.name !== 'home'">
-          <router-link to="/">Home</router-link>
-        </li>
-      </transition>
-      <li>
-        <router-link to="/website">Website</router-link>
-      </li>
-      <li>
-        <router-link to="/marketing">Marketing</router-link>
-      </li>
-      <li>
-        <router-link to="/interactive">Interactive / 3d</router-link>
-      </li>
-    </ul>
+	<nav id="nav" :class="[ { ['menu-open']: menuOpen } ]" >
 
-  </nav>
+		<ul>
 
+			<transition name="fade">
+
+				<li
+					v-if="menuOpen"
+					@click="toggleMenu()"
+					:class="['dark']"
+				>
+					<button>Close / Esc (x)</button>
+				</li>
+
+			</transition>
+
+			<transition name="fade">
+
+				<li v-if="$route.name !== 'home'"
+					@click="$router.push('/')"
+				>
+
+					<router-link to="/">Home</router-link>
+
+				</li>
+
+			</transition>
+
+			<li @click="$router.push('/website')" >
+
+				<router-link to="/website">Website</router-link>
+
+			</li>
+
+			<li @click="$router.push('/marketing')" >
+
+				<router-link to="/marketing">Marketing</router-link>
+
+			</li>
+
+			<li @click="$router.push('/interactive')" >
+
+				<router-link to="/interactive">Interactive / 3d</router-link>
+
+			</li>
+
+		</ul>
+
+	</nav>
+	</div>
 </template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			menuOpen: false
+		};
+	},
+	watch: {
+		'$route'(r) {
+			if ( this.menuOpen ) {
+				this.toggleMenu();				
+			}
+		},
+		'menuOpen'(o) {
+
+			if ( o ) {
+				this.keyPress();
+			} else {
+				window.onkeydown = null;
+			}
+
+		},
+	},
+	methods: {
+		keyPress() {
+
+			window.onkeydown = ( event ) => {
+			    if ( event.keyCode == 27 ) {
+			    	this.toggleMenu();
+			    }
+			};
+
+		},
+		button() {
+			this.toggleMenu();
+		},
+		toggleMenu() {
+			this.menuOpen = !this.menuOpen;			
+		}
+	}
+};
+
+</script>
 
 <style lang="scss" scoped>
 
-  // STYLE
+	#nav {
 
-  #nav {
+		order: 1;
+		margin-bottom:24px;
 
-    order: 1;
-    margin-bottom:48px;
+	}
 
-    @media only screen and (min-width:1000px) {
-      order: 0;
-    }
+	ul {
+		margin:0;
+		display: flex;
+		flex-direction:column;
+	}
 
-    a {
-      display:inline-block;
-      padding: 0.75rem 4rem;
-      font-weight: bold;
-      color: #2c3e50;
-      text-decoration:none;
-      &.router-link-exact-active {
-        color:Purple;
-      }
-    }
+	li {
 
-  }
+		text-align: center;
+		background:darken(white, 5%);
+		list-style:none;
+		cursor: pointer;
+		transition: background 200ms ease;
+		&:hover {
+			background: darken(white, 8%);
+		}
 
-  ul {
-    margin:0;
-  }
+		button, a {
+			display:inline-block;
+			padding: 0.75rem 4rem;
+			font-weight: bold;
+			text-decoration:none;
+		}
 
-  li {
-    text-align: center;
-    background:darken(white, 5%);
-    list-style:none;
-    &:nth-last-child(1) {
-      border-right:0;
-    }
+		button {
+			background: transparent;
+			outline: none;
+			border:0;
+			cursor: pointer;
+		}
 
-    &:nth-child(even) {
-      background:darken(white,2%);
-    }
+		a {
+			color: #2c3e50;
+			transition: color 200ms ease-in;
 
-  }
+			&:hover {
+				color: darken(aqua, 25%);
+			}
+		}
 
-  small {
-    display:block;
-    background:darken(white, 85%);
-    color:darken(white, 10%);
-    padding:8px 16px;
-  }
+		a.router-link-exact-active {
+			color:Purple;
+		}
+		
 
-  @media only screen and ( max-width: 630px ) {
+		&:nth-last-child(1) {
+			border-right:0;
+		}
 
-    li {
-      text-align: left;
-      padding-left: 18px;
-    }
+		&:nth-child(even) {
+			background:darken(white,2%);
+			&:hover {
+				background: darken(white,8%);
+			}
+		}
 
-  }
+	}
 
-  // GRID
+	li.dark {
+		background: black;
+		button {
+			color: white;
+		}
+	}
 
-  ul {
-    display: flex;
-    flex-direction:column;
-  }
+	small {
+		display:block;
+		background:darken(white, 85%);
+		color:darken(white, 10%);
+		padding:8px 16px;
+	}
 
-  @media only screen and ( min-width: 630px ) {
+	@keyframes opacityIn {
+		100% { opacity: 1; }
+	}
 
-    ul {
+	.external {
 
-      flex-direction:row;
+		cursor: pointer;
+		opacity:0;
+		background: black;
 
-    }
+		color: white;
+		font-size: 12px;
+		font-weight: normal;
 
-  }
+		padding: 16px 16px;
+		margin: 32px;
+		margin-top: 24px;
+		margin-bottom: 0;
 
-  @media only screen and ( min-width: 1000px ) { 
-    ul {
-      justify-content:center;
-    }
-  }
+		animation-name: opacityIn;
+		animation-duration: 500ms;
+		animation-delay: 0s;
+		animation-fill-mode: forwards;
+		animation-timing-function: ease-in;
+
+		&.menu-open {
+			pointer-events: none;
+		}
+
+	}
+
+	// large up
+
+	@media only screen and (min-width:1000px) {
+
+		.external {
+			opacity:0;
+			display:none;
+		}
+
+		li.dark {
+			display: none;
+		}
+
+		ul {
+			order: 0;
+			flex-direction:row;
+			justify-content:center;
+		}
+
+	}
+
+	// small only
+
+	@media only screen and ( max-width: 1000px ) {
+
+		#nav {
+
+			position: fixed;
+			top:0;
+			left:0;
+			height:100%;
+			width:100%;
+			z-index:100;
+			background: rgba(gray,1);
+			filter:blur(10px);
+			opacity:0;
+			transform: translate3d(0,-100%,0);
+			transition: transform 100ms ease-out;
+			transition-property: transform, filter, opacity;
+			transition-delay: 50ms, 0s, 30ms;
+
+			&.menu-open {
+				transform: translate3d(0,0,0);
+				opacity:1;
+				filter:blur(0px);
+				transition-timing-function: ease-in;
+				transition-delay: 0s, 50ms, 20ms;
+				li a, li button {
+					padding: 1.5rem 4rem;
+				}
+			}
+
+		}
+
+		li {
+			text-align: left;
+			padding-left: 18px;
+		}
+
+	}
 
 </style>
