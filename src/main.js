75ff75340e74a7c7430a093c23f1e1ac2f3c309c
Shawn Naquin <shawn.naquin@gmail.com>
2018-11-15 11:14:00 -0600

    - intersectionObserver polyfill added;

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import "intersection-observer";
+
+Vue.use(VLazyImagePlugin);
 
 Vue.config.productionTip = false;
 
