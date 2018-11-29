d83565b089bd1717924c937e1b1f11fcabae9abe
shawnnaquin <shawn.naquin@gmail.com>
2018-11-29 13:10:30 -0600

    adding heroku contact form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,29 @@
   	<router-link to="/website" :class="['external']" >
   		View More
   	</router-link>
+
+  	<form action="https://shawns-contact.form.herokuapp.com/send_email" method="POST">
+  		<div class="form-group">
+  			<label>Name</label>
+  			<input type="text" name="name" class="form-control" placeholder="Name" required>
+  		</div>
+  		<div class="form-group">
+  			<label>Your Email</label>
+  			<input type="email" name="email" class="form-control" placeholder="Your E-mail" required>
+  		</div>
+  		<div class="form-group">
+  			<label>Subject</label>
+  			<input type="text" name="subject" class="form-control" placeholder="Subject" required>
+  		</div>
+  		<div class="form-group">
+  			<textarea class="form-control" name="message" rows="7" placeholder="Message" required>
+  			</textarea>
+  		</div>
+  		<div class="text-center">
+  			<input type="submit" value="Submit" class="btn btn-primary">
+  		</div>
+  	</form>
+
   </div>
 
 </template>
