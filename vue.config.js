196f257cfcf660d000fbdfec1dcb4b70f1f891e4
shawnnaquin <shawn.naquin@gmail.com>
2018-12-04 20:21:57 -0600

    added critical plugin and preload for webpack through vue-cli

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,32 @@
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-	baseUrl: '/'
-}
+	baseUrl: '/',
+	pluginOptions: {
+	    critical: {
+	        inline: true,
+	        width: 1024,
+	        height: 565,
+	    }
+	},
+	configureWebpack: {
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					use: [
+					  process.env.NODE_ENV !== 'production'
+					    ? 'vue-style-loader'
+					    : MiniCssExtractPlugin.loader,
+					  'css-loader'
+					]
+				}
+			]
+		},
+
+		plugins: [
+			new MiniCssExtractPlugin()
+		]
+	}
+
+};
