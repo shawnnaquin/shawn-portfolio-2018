a9903ed630e813850199176a71c1c75635ed35b9
Shawn Naquin <shawn.naquin@gmail.com>
2019-01-17 12:15:25 -0600

    removed some stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,35 +3,16 @@
 
 module.exports = function(grunt) {
   grunt.initConfig({
-    responsive_images: {
-      dev: {
-        options: {
-          engine: "gm",
-          upscale: false,
-          sizes: [
-            { name: "sm", suffix: "_1x", quality: 60, width: 600 },
-            { name: "sm", suffix: "_2x", quality: 60, width: 1200 },
-            { name: "md", suffix: "_1x", quality: 60, width: 900 },
-            { name: "md", suffix: "_2x", quality: 60, width: 1800 },
-            { name: "lg", suffix: "_1x", quality: 60, width: 1440 },
-            { name: "lg", suffix: "_2x", quality: 60, width: 2880 }
-          ]
-        },
-        files: [
-          {
-            expand: true,
-            src: ["**/*.{jpg,png}"],
-            cwd: "./src/assets/portfolio/",
-            dest: "./public/img/portfolio/"
-          }
-        ]
-      }
-    },
     cwebp: {
       dynamic: {
         options: {
           q: 60
         },
+        sizes: [
+          { name: "sm", suffix: "_2x", quality: 60, width: 1200 },
+          { name: "md", suffix: "_2x", quality: 60, width: 1800 },
+          { name: "lg", suffix: "_2x", quality: 60, width: 2880 }
+        ],
         files: [
           {
             expand: true,
