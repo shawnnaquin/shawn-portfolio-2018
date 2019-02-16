00f9dc2e3f266dcdca3925e962349c6b829f73ca
shawnnaquin <shawn.naquin@gmail.com>
2019-02-16 02:05:42 -0600

    try this

--- public/js/extend-sw.js
+++ public/js/extend-sw.js
@@ -1,39 +1,4 @@
 // self.addEventListener('activate', function(event) {});
-this.addEventListener('fetch', function(event) {
-
-    // Full documentation for respondWith is available on
-    // MDN (http://mzl.la/1SKtV92), but basically with this
-    // you are able to customize the response from the
-    // request you initially get by the browser.
-
-    event.respondWith(
-
-    // caches.open look familiar? It should! We just used
-    // it in the code above! Here we are finding a match
-    // for the event.request in our cached v1 storage (in
-    // the browser).
-    //
-    // If we find a match for the request in the cache
-    // storage, that means our service worker will serve
-    // that file right up from the browser itself rather
-    // than going alllll the way to the server to get it!
-    // NICE!!!
-
-    // However, if the resource isn't found, then it WILL
-    // go ALLLL the way to the server to grab it, or if
-    // it's in offline mode, will break and not show the
-    // file. Bummer!
-
-        caches.open('shawn-portfolio-2018-precache-https://www.devnola.com/').then(function(cache) {
-            return cache.match(event.request).then(function(response) {
-                return response || fetch(event.request).then(function(response) {
-                    cache.put(event.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
-});
 
 self.addEventListener('message', event => {
 
--- vue.config.js
+++ vue.config.js
@@ -8,9 +8,29 @@ module.exports = {
     appleMobileWebAppStatusBarStyle: 'black',
     workboxOptions: {
       importWorkboxFrom: "local",
+      exclude: [/(.*?)/]
       importScripts: ['/js/extend-sw.js'],
       navigateFallback: '/',
-      directoryIndex: "index.html"
+      directoryIndex: "index.html",
+      runtimeCaching: [{
+          // Match any same-origin request that contains 'api'.
+          urlPattern: /(.*?)/,
+          handler: 'cacheFirst',
+          options: {
+            networkTimeoutSeconds: 1,
+            cacheName: '2019-portfolio-shawn',
+          },
+        }, {
+          // To match cross-origin requests, use a RegExp that matches
+          // the start of the origin:
+          urlPattern: new RegExp('^https://cors\.example\.com/'),
+          handler: 'staleWhileRevalidate',
+          options: {
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }]
     }
   }
 };
