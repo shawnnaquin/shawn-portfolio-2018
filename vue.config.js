7f5b552d50204b278817db605ebc9a80d88b4696
shawnnaquin <shawn.naquin@gmail.com>
2019-02-16 02:17:31 -0600

    remove cors

--- vue.config.js
+++ vue.config.js
@@ -19,17 +19,7 @@ module.exports = {
           options: {
             cacheName: '2019-portfolio-shawn'
           },
-        }, {
-          // To match cross-origin requests, use a RegExp that matches
-          // the start of the origin:
-          urlPattern: new RegExp('^https://cors\.example\.com/'),
-          handler: 'staleWhileRevalidate',
-          options: {
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        }]
+      }]
     }
   }
 };
