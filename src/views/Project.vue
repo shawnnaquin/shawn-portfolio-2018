4227976bf3f8e7aa7d7153b3f04b267f77c47f2b
shawnnaquin <shawn.naquin@gmail.com>
2019-02-18 13:49:27 -0600

    reset scrollTop on project

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -516,7 +516,7 @@ export default {
       setTimeout(() => {
         this.showBlurb = true;
         document.body.style.height = "";
-        this.$scrollTo(":root", 0);
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
         done();
       }, 100);
     },
